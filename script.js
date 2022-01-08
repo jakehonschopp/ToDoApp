6643a17956943b449a7a76e603ee649d5e86f2ed
Jake Honschopp <Jakehonschopp@yahoo.com>
2022-01-08 11:17:11 -0500

    filling in instructions to make code easier to read

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+// Here's where we select all the items in the HTML 
+
 const todoForm = document.querySelector('.todo-form');
 const todoInput = document.querySelector('.todo-input');
 const todoItemsList = document.querySelector('.todo-items');
 
 let todos = [];
 
+// making sure the page doesn't refresh upon submitting 
 todoForm.addEventListener('submit', function(event) {
   
   event.preventDefault();
   addTodo(todoInput.value); 
 });
 
-
+// here's where we add the todo item, if the item is not empty, we submit it with id, name and completed properties
 function addTodo(item) {
   if (item !== '') {
     const todo = {
@@ -25,7 +28,7 @@ function addTodo(item) {
     todoInput.value = '';
   }
 }
-
+// function to render the todos to the page
 function renderTodos(todos) {
   todoItemsList.innerHTML = '';
 
@@ -49,12 +52,14 @@ function renderTodos(todos) {
   });
 
 }
-
+// function to add the todos to localStorage so we can get them if they are unchecked. 
+// Make sure they are stored as strings (JSON.stringify)
 function addToLocalStorage(todos) {
   localStorage.setItem('todos', JSON.stringify(todos));
   renderTodos(todos);
 }
-
+// function to get said items from localStorage once they are stored
+// we have to parse them back to arrays from strings with JSON.parse
 function getFromLocalStorage() {
   const reference = localStorage.getItem('todos');
   if (reference) {
@@ -62,7 +67,7 @@ function getFromLocalStorage() {
     renderTodos(todos);
   }
 }
-
+// toggle function to the value either completed or not completed
 function toggle(id) {
   todos.forEach(function(item) {
     if (item.id == id) {
@@ -72,7 +77,7 @@ function toggle(id) {
 
   addToLocalStorage(todos);
 }
-
+// the function that can delete todos 
 function deleteTodo(id) {
   todos = todos.filter(function(item) {
     return item.id != id;
@@ -81,7 +86,9 @@ function deleteTodo(id) {
   addToLocalStorage(todos);
 }
 
-
+// here we must listen for the click and then decide what to do. If the user clicked on the .checkbox,
+// then call taggle function by passing the id.
+// if the user clicked on the .delete.button, then call the delete function by passing the id
 todoItemsList.addEventListener('click', function(event) {
   if (event.target.type === 'checkbox') {
     toggle(event.target.parentElement.getAttribute('data-key'));
@@ -92,7 +99,7 @@ todoItemsList.addEventListener('click', function(event) {
   } 
 }); 
 
-
+// function to fill the todos if localStorage is empty upon page load
 function fillEmptyTodos() {
   axios.get("https://jsonplaceholder.typicode.com/todos/")
       .then(response => {
@@ -103,9 +110,10 @@ function fillEmptyTodos() {
       }
     })
      .catch(error => console.error(error)); 
-};   
+}    
  
-
+// event listener that works on page load, and will fire the fillEmptyTodos function if localStorage
+// is empty 
 window.addEventListener("load", function() { 
   console.log(localStorage.getItem("todos") === '[]');
   if(!localStorage.getItem("todos") || localStorage.getItem("todos") === '[]') {
